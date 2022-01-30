71b914d5ceb5ece896628a412edd77d7f21990c8
Aboba <evelina@mircod.com>
2022-01-30 20:44:53 +0300

    Added useSelectors, tsConfigand changed some files on ts, start filter

--- src/components/Tickets/index.js
+++ src/components/Tickets/index.js
@@ -1,8 +1,16 @@
 import { Ticket } from "./Ticket";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
-export const TicketsList = () => {
-  const tickets = useSelector((state) => state.tickets);
+export const TicketsList = ({ compainFilter }) => {
+  const allTickets = useSelector((state) => state.tickets);
+  const compains = useSelector((state) => state.compains);
+  let tickets = [];
+
+  //   useEffect({
+  //       if (compainFilter !== 'all') tickets = allTickets.filter(ticket => ticket)
+
+  //   }, [compainFilter]);
   return (
     <>
       {tickets.map((ticket) => (
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import ticketsReducer from "../store/Tickets";
+import ticketsReducer from "./tickets";
+import segmentsReducer from "./segments";
+import compainsReducer from "./compains";
 
 export default configureStore({
   reducer: {
     tickets: ticketsReducer,
+    segments: segmentsReducer,
+    compains: compainsReducer,
   },
 });
